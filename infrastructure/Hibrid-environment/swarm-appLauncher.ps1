ae4818df498a30ee055e2b9e4d620c4599dd637a
Daniel Rodriguez <daniel.rdriguez.rdriguez@gmail.com>
2018-05-13 03:07:53 +0200

    hadoop tweaking

--- infrastructure/Hibrid-environment/swarm-appLauncher.ps1
+++ infrastructure/Hibrid-environment/swarm-appLauncher.ps1
@@ -9,12 +9,13 @@
 $managerZero="vmNode1"
 $services = docker-machine ssh $managerZero "docker ps -a"
 $hadoopContainer = ($services -match "^[\S]+(?=[\s]+sequenceiq\/hadoop-docker)" -split '\s+')[0]
+docker-machine ssh $managerZero "docker exec $hadoopContainer /usr/local/hadoop/bin/hdfs dfsadmin -safemode leave"
 docker-machine ssh $managerZero "docker exec $hadoopContainer /usr/local/hadoop/bin/hdfs dfs -mkdir /data"
 docker-machine ssh $managerZero "docker exec $hadoopContainer /usr/local/hadoop/bin/hdfs dfs -mkdir /results"
 docker-machine ssh $managerZero "docker exec $hadoopContainer /usr/local/hadoop/bin/hdfs dfs -put /usr/local/hadoop/data/DelightingCustomersBDextract2Formatted.json /data"
 
 
 $sparkContainer = ($services -match "^[\S]+(?=[\s]+danielrodriguez\/docker-spark)" -split '\s+')[0]
-docker-machine ssh $managerZero "docker exec $sparkContainer /usr/hadoop-2.7.1/bin/hdfs dfs -ls hdfs://hadoop:9000/data/"
-docker-machine ssh $managerZero """docker exec $sparkContainer spark-submit --class "thesisApp.ThesisAppLauncher" --deploy-mode client --master spark://spark-master:7077 app/thesisapp_2.11-1.0.jar "hdfs://hadoop:9000" "/data/" "DelightingCustomersBDextract2Formatted.json" "/results/" "app/dataExplorer_sample" "app/dataExplorer_sample2" """
+docker-machine ssh $managerZero "docker exec $sparkContainer /usr/hadoop-2.7.1/bin/hdfs dfs -ls hdfs://hadoop-master:9000/data/"
+docker-machine ssh $managerZero """docker exec $sparkContainer spark-submit --class "thesisApp.ThesisAppLauncher" --deploy-mode client --master spark://spark-master:7077 app/thesisapp_2.11-1.0.jar "hdfs://hadoop-master:9000" "/data/" "DelightingCustomersBDextract2Formatted.json" "/results/" "app/dataExplorer_sample" "app/dataExplorer_sample2" """
 
